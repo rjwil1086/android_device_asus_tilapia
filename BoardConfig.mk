9db3ae274466d926e260bd6f96c50c837c6ac4f1
Ryan Wilson <rjwil1086@gmail.com>
2014-12-01 21:44:00 -0600

    TWRP-ify and add F2FS Support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,3 +48,13 @@ include device/asus/grouper/BoardConfigCommon.mk
 TARGET_RECOVERY_FSTAB = device/asus/tilapia/fstab.grouper
 
 MALLOC_IMPL := dlmalloc
+
+#TWRP
+DEVICE_RESOLUTION := 800x1280
+BOARD_HAS_NO_REAL_SDCARD := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_NO_USB_STORAGE := false
+TW_INCLUDE_JB_CRYPTO := true
+
+# F2FS Support in TWRP
+TARGET_USERIMAGES_USE_F2FS := true
